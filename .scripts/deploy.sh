55417f5003bc5d71854477e548fcf3f4b0c4a636
robikurniawan <robbykurniawan.rpl@gmail.com>
2022-06-10 14:39:50 +0800

    deploy

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,31 +1,29 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-echo "Deployment started ..."
+echo "Deploying application ..."
 
-# Enter maintenance mode or return true
-# if already is in maintenance mode
-(php artisan down) || true
+# Enter maintenance mode
+(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+    # Update codebase
+    git fetch origin deploy
+    git reset --hard origin/deploy
 
-# Pull the latest version of the app
-git pull origin production
+    # Install dependencies based on lock file
+    composer install --no-interaction --prefer-dist --optimize-autoloader
 
-# Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
+    # Migrate database
+    php artisan migrate --force
 
-# Clear the old cache
-php artisan clear-compiled
+    # Note: If you're using queue workers, this is the place to restart them.
+    # ...
 
-# Recreate cache
-php artisan optimize
+    # Clear cache
+    php artisan optimize
 
-# Compile npm assets
-npm run prod
-
-# Run database migrations
-php artisan migrate --force
-
+    # Reload PHP to update opcache
+    echo "" | sudo -S service php7.4-fpm reload
 # Exit maintenance mode
 php artisan up
 
-echo "Deployment finished!"
+echo "Application deployed!"
