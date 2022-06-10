72b7aac6cd6489da5052ce6e62c9ed8c576263e3
robikurniawan <robbykurniawan.rpl@gmail.com>
2022-06-10 14:55:03 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "Deploying application ..."
+vendor/bin/phpunit
 
-# Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
-    # Update codebase
-    git fetch origin deploy
-    git reset --hard origin/deploy
+(git push) || true
 
-    # Install dependencies based on lock file
-    composer install --no-interaction --prefer-dist --optimize-autoloader
+git checkout production
+git merge master
 
-    # Migrate database
-    php artisan migrate --force
+git push origin production
 
-    # Note: If you're using queue workers, this is the place to restart them.
-    # ...
-
-    # Clear cache
-    php artisan optimize
-
-    # Reload PHP to update opcache
-    echo "" | sudo -S service php7.4-fpm reload
-# Exit maintenance mode
-php artisan up
-
-echo "Application deployed!"
+git checkout master
