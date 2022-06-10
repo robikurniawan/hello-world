4608edd39dded72c6b0411839cd0139c10f17013
robikurniawan <robbykurniawan.rpl@gmail.com>
2022-06-10 15:12:13 +0800

    deploy

--- server_deploy.sh
+++ server_deploy.sh
@@ -6,8 +6,8 @@ echo "Deploying application ..."
 # Enter maintenance mode
 (php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
     # Update codebase
-    git fetch origin deploy
-    git reset --hard origin/deploy
+    git fetch origin production
+    git reset --hard origin/production
 
     # Install dependencies based on lock file
     composer install --no-interaction --prefer-dist --optimize-autoloader
